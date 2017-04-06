33092800c1bb7e2f07781535174260cca3996eb4
Ant0n-G <GadunAnton@gmail.com>
2017-04-06 13:32:56 -0700

    Update GitClass.java

--- ProjectGit/src/GitPackage/GitClass.java
+++ ProjectGit/src/GitPackage/GitClass.java
@@ -5,6 +5,8 @@ public class GitClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git Hub");
 		System.out.println("Second Git hello ");
+		System.out.println("Hello from Git ");
+		
 
 	}
 
